032ee945ed95ec50e0609fc27ceb853b10f81ea9
denuwan0 <denuwan0@gmail.com>
2024-02-20 20:14:50 +0530

    shopping web modify

--- application/views/Template/Header.php
+++ application/views/Template/Header.php
@@ -37,12 +37,12 @@
     <div class="container-fluid">
         <div class="row bg-secondary py-1 px-xl-5">
             <div class="col-lg-6 d-none d-lg-block">
-                <div class="d-inline-flex align-items-center h-100">
+                <!--div class="d-inline-flex align-items-center h-100">
                     <a class="text-body mr-3" href="">About</a>
                     <a class="text-body mr-3" href="">Contact</a>
                     <a class="text-body mr-3" href="">Help</a>
                     <a class="text-body mr-3" href="">FAQs</a>
-                </div>
+                </div-->
             </div>
             <div class="col-lg-6 text-center text-lg-right">
                 <div class="d-inline-flex align-items-center">
